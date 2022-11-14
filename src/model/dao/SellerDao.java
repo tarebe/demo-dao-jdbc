6509445cca856659525abd4f53a1b7ec50294c29
Tarebe Sousa <tarebehenrique123@gmail.com>
2022-11-14 14:19:51 -0300

    SellerDaoJDBC e DaoFacotry

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -1,5 +1,15 @@
 package model.dao;
 
-public interface SellerDao {
+import java.util.List;
 
+import model.entities.Seller;
+
+public interface SellerDao {
+	
+	void insert(Seller obj); //fara a inserção
+	void update(Seller obj); //fara a atualização
+	void deleteById(Integer id); //Deletara por ID
+	Seller findById(Integer id); //Verifica se o ID existe
+	List<Seller> findAll(); //Retorna todos os Sellers
+	
 }
